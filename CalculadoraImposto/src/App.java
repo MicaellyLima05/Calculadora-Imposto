92a2cb5aa92abf1cd2aef147465a490ea2f804df
micaelly <lima.mica.2005@gmail.com>
2025-09-22 17:04:32 -0300

    adicionando menu

--- CalculadoraImposto/src/App.java
+++ CalculadoraImposto/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //configura leitor 
+        Scanner usuario = new Scanner(System.in);
+        usuario.useLocale(Locale.US);
+
+        System.out.println("\n///Calculadora de Impostos///\n");
+        System.out.print("Qual nome do Produto? = ");
+        String nomeProduto = usuario.nextLine();
+
+        System.out.print("Qual o valor (USD) do Produto? = US$");
+        double valorBrutoProduto = usuario.nextDouble();
+
+        System.out.print("Qual o valor do frete (USD) do Produto? = US$");
+        double valorBrutoFrete = usuario.nextDouble();
+
+        System.out.print("Qual a origem do Produto? ");
+        
+        usuario.close();
+
     }
+
 }
